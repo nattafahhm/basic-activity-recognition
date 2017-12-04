c9cb45fb171d3634d05131e5927e40df04a8a3a7
nattayam <nattafahh@gmail.com>
2017-12-04 13:05:33 +0900

    ignore some files

--- lib/myplclust.R
+++ lib/myplclust.R
@@ -1,13 +1,4 @@
 myplclust <- function( hclust, lab=hclust$labels, lab.col=rep(1,length(hclust$labels)), hang=0.1,...){
- ## modifiction of plclust for plotting hclust objects *in colour*!
- ## Copyright Eva KF Chan 2009
- ## Arguments:
- ##    hclust:    hclust object
- ##    lab:        a character vector of labels of the leaves of the tree
- ##    lab.col:    colour for the labels; NA=default device foreground colour
- ##    hang:     as in hclust & plclust
- ## Side effect:
- ##    A display of hierarchical cluster with coloured leaf labels.
  y <- rep(hclust$height,2)
  x <- as.numeric(hclust$merge)
  y <- y[which(x<0)]
